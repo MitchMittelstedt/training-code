20bd5dd2a58c4ea94f0f0e7feddd6847ba5c4fba
mitch mittelstedt <mmittelstedt@gmail.com>
2019-10-24 17:13:53 -0500

    add MediaWorld

---
+++ 00_csharp/MediaWorld/MediaWorld.Client/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MediaWorld.Client
+{
+  /// <summary>
+  /// 
+  /// </summary>
+    internal class Program
+    {
+      /// <summary>
+      /// 
+      /// </summary>
+        private static void Main()
+        {
+            var helper = MediaSingleton.GetInstance();
+            Console.WriteLine(helper);
+        }
+    }
+}
---
+++ 00_csharp/MediaWorld/MediaWorld.Domain/MediaSingleton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MediaWorld.Domain.Models
+{
+    public class MediaSingleton
+    {
+      private static readonly string _instance = "mediaplayer";
+      
+      private  MediaSingleton() {}
+
+      public static string GetInstance() 
+      {
+        return _instance;
+
+      }
+    }
+}
