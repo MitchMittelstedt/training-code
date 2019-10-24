db9df009317ea8f47c95da92efa3c0abe43929ba
mitch mittelstedt <mmittelstedt@gmail.com>
2019-10-24 17:22:33 -0500

    add HelloWorld

---
+++ 00_csharp/HelloWorld/HelloWorld.Client/Program.cs
@@ -0,0 +1,20 @@
+﻿using sc = System.Console; //import with alias
+using System;
+
+namespace HelloWorld.Client
+{
+  /// <summary>
+  /// contains the start of the program
+  /// </summary>
+    internal class Program
+    {
+      /// <summary>
+      /// starts the program
+      /// </summary>
+        private static void Main()
+        {
+            sc.WriteLine("Hello World!");
+        }
+    }
+}
+
