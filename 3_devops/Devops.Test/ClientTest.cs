f5a40b2975861298f655e9bd79d0c9c33cd80068
mitch mittelstedt <mmittelstedt@gmail.com>
2019-12-10 10:03:00 -0600

    add 2 tests

---
+++ 3_devops/Devops.Test/ClientTest.cs
@@ -0,0 +1,38 @@
+using Devops.Client.Controllers;
+using Xunit;
+using Microsoft.Extensions.Logging;
+
+namespace Devops.Test
+{
+  public class ClientTest
+  {
+    private readonly ILogger<HomeController> logger = LoggerFactory.Create(o => o.SetMinimumLevel(LogLevel.Debug)).CreateLogger<HomeController>();
+    [Fact]
+    public void Test_IndexPage()
+    {
+      //arrange
+      var home = new HomeController(logger);
+
+      //act
+      var index = home.Index();
+
+      //assert
+      Assert.NotNull(index);
+
+    }
+
+        [Fact]
+    public void Test_PrivacyPage()
+    {
+    //arrange
+    var home = new HomeController(logger);
+
+    //act
+    var privacy = home.Privacy();
+
+    //assert
+    Assert.NotNull(privacy);
+
+    }
+  }
+}
